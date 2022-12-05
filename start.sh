ac01c84fc928029370563a7ac6da1b315501c8f4
Elseso <117286044+Elseso@users.noreply.github.com>
2022-12-06 04:06:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Elseso/Advance-EvaMaria
+  git clone https://github.com/Greymattersbot/Advance-EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
